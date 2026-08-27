65d5b16cc6c0f6fb3bcd7b7c5e920af280e02ce9
fatougu2007-beep <fatougu2007@gmail.com>
2026-08-27 17:12:20 +0000

    Mon premier traducteur Wolof

---
+++ traducteur.py
@@ -0,0 +1,19 @@
+print("===TRADUCTEUR WOLOF===")
+print("tape 'quitter' pour sortir ")
+while True:
+    mot=input ("\nÉcrit un mot en français :")
+    if mot=='quitter':
+    print ("jërejëf ! ba beneen")
+    break 
+    elif mot =="bonjour":
+    print(" En Wolof : Nanga def")
+    elif mot =="merci":
+    print ("En Wolof :jërejëf")
+    elif mot =="comment":
+    print("En Wolof :naka nga def")
+    elif mot =="oui":
+    print("En wolof :Waaw")
+    elif mot =="non":
+    print ("En Wolof:Déedéet")
+    else:
+    print("je ne connais pas ce mot encore.jërejëf!") 
